2a55b1fbbc11b10951fac061f0eaaf46873faee6
Shruti <shrutigawande1910@gmail.com>
2024-08-10 12:50:08 +0530

    style: Header

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,12 +1,28 @@
-import Header from '@/Components/Blog/Header/Header'
-import React from 'react'
+import FaqSection from "@/Components/Blog/FaqSection/FaqSection";
+import Header from "@/Components/Blog/Header/Header";
+import LatestPost from "@/Components/Blog/LatestPost/LatestPost";
+import PostList from "@/Components/Blog/PostList/PostList";
+import Slider from "@/Components/Blog/Slider/Slider";
+import SubscribeSection from "@/Components/Blog/SubscribeSection/SubscribeSection";
+import WhoIsOutspokn from "@/Components/Blog/WhoIsOutspokn/WhoIsOutspokn";
+import Footer from "@/Components/Footer/Footer";
+import Navbar from "@/Components/Navbar/Navbar";
+import React from "react";
 
 const index = () => {
   return (
     <div>
-      <Header/>
+      <Navbar />
+      <Header />
+      <Slider />
+      <LatestPost />
+      <PostList />
+      <WhoIsOutspokn />
+      <FaqSection />
+      <SubscribeSection />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default index;
