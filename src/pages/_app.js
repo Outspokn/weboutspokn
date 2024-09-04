df118dd1e191f7cb552771ccbe1dbddce5e5a4b0
somnathraz <somnathkhadanga810@gmail.com>
2024-09-04 16:40:36 +0530

    complete center part

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import { Nunito_Sans } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import styles from "@/styles/Home.module.css";
 const nunitoSans = Nunito_Sans({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
     <main className={nunitoSans.className}>
+      <SpeedInsights />
       <Component {...pageProps} />
     </main>
   );
