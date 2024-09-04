2ba0bf7e191da4c99dea1dc5ac794edc5414aac1
somnathraz <somnathkhadanga810@gmail.com>
2024-09-04 16:59:13 +0530

    complete center part

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="dY9ZF08mhZQpo4vWFxSGfFOV37zRNGJiv1idb4i2mX8"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
