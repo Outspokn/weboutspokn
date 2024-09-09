f6598a4160a1a823bb4284cde9a8640aeee97935
Shruti <shrutigawande1910@gmail.com>
2024-09-10 00:04:57 +0530

    refactor: Removed Search Feature

--- src/Components/Blog/Header/Header.js
+++ src/Components/Blog/Header/Header.js
@@ -1,11 +1,12 @@
 import { useEffect, useState, useMemo } from "react";
 import styles from "./Header.module.css";
-import { ImTwitter, ImYoutube, ImSearch } from "react-icons/im";
+import { FaYoutube } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 import Link from "next/link";
 
 const Header = ({ posts, onFilter }) => {
   const [activeCategory, setActiveCategory] = useState("All");
-  const [searchTerm, setSearchTerm] = useState("");
+  // const [searchTerm, setSearchTerm] = useState("");
 
   const categories = useMemo(
     () => [
@@ -20,8 +21,8 @@ const Header = ({ posts, onFilter }) => {
                 "Other",
                 "Career",
                 "Interview Question",
-                "Business Analyst",
-                "Business Analytics",
+                // "Business Analyst",
+                // "Business Analytics",
               ].includes(tag)
           )
       ),
@@ -39,14 +40,15 @@ const Header = ({ posts, onFilter }) => {
       );
     }
 
-    if (searchTerm) {
-      const lowerCaseSearchTerm = searchTerm.toLowerCase();
-      filtered = filtered.filter((post) =>
-        post.tag.toLowerCase().includes(lowerCaseSearchTerm)
-      );
-    }
+    // if (searchTerm) {
+    //   const lowerCaseSearchTerm = searchTerm.toLowerCase();
+    //   filtered = filtered.filter((post) =>
+    //     post.tag.toLowerCase().includes(lowerCaseSearchTerm)
+    //   );
+    // }
     return filtered;
-  }, [searchTerm, activeCategory, posts]);
+  }, [activeCategory, posts]);
+  // [searchTerm, activeCategory, posts]);
 
   useEffect(() => {
     onFilter(filteredPosts);
@@ -54,7 +56,7 @@ const Header = ({ posts, onFilter }) => {
 
   const handleCategoryClick = (category) => {
     setActiveCategory(category);
-    setSearchTerm("");
+    // setSearchTerm("");
   };
 
   return (
@@ -64,16 +66,20 @@ const Header = ({ posts, onFilter }) => {
           <h1 className={styles.title}>Discover Nice Articles Here</h1>
           <div className={styles.socialIcons}>
             <Link
-              href="https://twitter.com"
+              href="https://twitter.com/skillslash"
+              target="_blank"
+              aria-label="Go to the Twitter Page of skillslash"
               className={`${styles.icon} ${styles.twitter}`}
             >
-              <ImTwitter />
+              <FaXTwitter className={styles.FIcon} />
             </Link>
             <Link
-              href="https://youtube.com"
+              href="https://www.youtube.com/c/Skillslash"
+              target="_blank"
+              aria-label="Go to the Youtube of skillslash"
               className={`${styles.icon} ${styles.youtube}`}
             >
-              <ImYoutube />
+              <FaYoutube className={styles.FIcon} />
             </Link>
           </div>
         </div>
@@ -87,7 +93,7 @@ const Header = ({ posts, onFilter }) => {
           Quickly And Without Any Problems.
         </p>
         <div className={styles.searchContainer}>
-          <div className={styles.searchBar}>
+          {/* <div className={styles.searchBar}>
             <ImSearch className={styles.searchIcon} />
             <input
               type="text"
@@ -95,7 +101,7 @@ const Header = ({ posts, onFilter }) => {
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
             />
-          </div>
+          </div> */}
           <div className={styles.categories}>
             {categories.map((category) => (
               <button
