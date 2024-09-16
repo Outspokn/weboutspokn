8ce6de5a5ecff1e3fbd406d11dc26b74903cc86d
Shruti <shrutigawande1910@gmail.com>
2024-09-16 17:18:55 +0530

    feat: social media icons

--- src/Components/BlogPage/MainContent/MainContent.js
+++ src/Components/BlogPage/MainContent/MainContent.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import styles from "./MainContent.module.css";
 import { IoLogoWhatsapp } from "react-icons/io5";
 import Image from "next/image";
@@ -9,6 +9,31 @@ import Link from "next/link";
 const MainContent = ({ post }) => {
   const contentRefs = useRef([]);
   const articleRef = useRef(null);
+  const [shareUrls, setShareUrls] = useState({});
+
+  useEffect(() => {
+    if (post && typeof window !== "undefined") {
+      const currentUrl = `${window.location.origin}${window.location.pathname}`;
+
+      setShareUrls({
+        whatsapp: `https://wa.me/?text=${encodeURIComponent(
+          post.title
+        )}%20${encodeURIComponent(currentUrl)}`,
+        twitter: `https://twitter.com/intent/tweet?url=${encodeURIComponent(
+          currentUrl
+        )}&text=${encodeURIComponent(post.title)}`,
+        linkedin: `https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(
+          currentUrl
+        )}&title=${encodeURIComponent(post.title)}&summary=${encodeURIComponent(
+          post.desc
+        )}&source=${encodeURIComponent(window.location.hostname)}`,
+
+        meta: `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+          currentUrl
+        )}`,
+      });
+    }
+  }, [post]);
 
   const scrollToSection = (index) => {
     const element = contentRefs.current[index + 1];
@@ -17,36 +42,17 @@ const MainContent = ({ post }) => {
     if (element && articleElement) {
       articleElement.scrollTo({
         top:
-          articleElement.scrollTop + element.getBoundingClientRect().top - 60,
+          articleElement.scrollTop + element.getBoundingClientRect().top - 90,
         behavior: "smooth",
       });
     }
   };
 
-  // useEffect(() => {
-  //   contentRefs.current = document.querySelectorAll("h2,h3");
-  // }, [post]);
-
   if (!post) return <p>Post not found!</p>;
-
-  // const shareUrls = {
-  //   whatsapp: `https://wa.me/?text=${encodeURIComponent(
-  //     post.title
-  //   )}%20${encodeURIComponent(window.location.href)}`,
-  //   twitter: `https://twitter.com/intent/tweet?url=${encodeURIComponent(
-  //     window.location.href
-  //   )}&text=${encodeURIComponent(post.title)}`,
-  //   linkedin: `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(
-  //     window.location.href
-  //   )}`,
-  //   meta: `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
-  //     window.location.href
-  //   )}`,
-  // };
 
   return (
     <div className={styles.container}>
-      <div className={styles.sideContainer}>
+      {/* <div className={styles.sideContainer}> */}
       <div className={styles.sidebar}>
         <h3>Table of Contents</h3>
         <ul className={styles.tocList}>
@@ -60,11 +66,10 @@ const MainContent = ({ post }) => {
             const url = `#${uMake}`;
 
             return (
-              <Link href={url}>
+              <Link href={url} key={index}>
                 <li
-                  key={index}
                   className={styles.tocItem}
-                  // onClick={() => scrollToSection(index)}
+                  onClick={() => scrollToSection(index)}
                 >
                   {item}
                 </li>
@@ -73,8 +78,8 @@ const MainContent = ({ post }) => {
           })}
         </ul>
       </div>
-      </div>
-     
+      {/* </div> */}
+
       <div className={styles.main} ref={articleRef}>
         <article className={styles.article}>
           <div className={styles.imgWrapper}>
@@ -96,54 +101,50 @@ const MainContent = ({ post }) => {
                       <span className={styles.date}>{post.date}</span>
                     </div>
                     <div className={styles.articleIconContainer}>
-                      {/* <FaMeta
-                        className={`${styles.articleIcon} ${styles.meta}`}
-                      /> */}
-                      {/* <FaXTwitter
-                        className={`${styles.articleIcon} ${styles.twitter}`}
-                      />
-                      <FaLinkedinIn
-                        className={`${styles.articleIcon} ${styles.linkedin}`}
-                      /> */}
-                      {/* <IoLogoWhatsapp
-                        className={`${styles.articleIcon} ${styles.whatsapp}`}
-                      /> */}
-                      {/* <a
-                        href={shareUrls.meta}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <FaMeta
-                          className={`${styles.articleIcon} ${styles.meta}`}
-                        />
-                      </a>
-                      <a
-                        href={shareUrls.whatsapp}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <IoLogoWhatsapp
-                          className={`${styles.articleIcon} ${styles.whatsapp}`}
-                        />
-                      </a>
-                      <a
-                        href={shareUrls.twitter}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <FaXTwitter
-                          className={`${styles.articleIcon} ${styles.twitter}`}
-                        />
-                      </a>
-                      <a
-                        href={shareUrls.linkedin}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <FaLinkedinIn
-                          className={`${styles.articleIcon} ${styles.linkedin}`}
-                        />
-                      </a> */}
+                      {shareUrls.meta && (
+                        <a
+                          href={shareUrls.meta}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <FaMeta
+                            className={`${styles.articleIcon} ${styles.meta}`}
+                          />
+                        </a>
+                      )}
+                      {shareUrls.whatsapp && (
+                        <a
+                          href={shareUrls.whatsapp}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <IoLogoWhatsapp
+                            className={`${styles.articleIcon} ${styles.whatsapp}`}
+                          />
+                        </a>
+                      )}
+                      {shareUrls.twitter && (
+                        <a
+                          href={shareUrls.twitter}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <FaXTwitter
+                            className={`${styles.articleIcon} ${styles.twitter}`}
+                          />
+                        </a>
+                      )}
+                      {shareUrls.linkedin && (
+                        <a
+                          href={shareUrls.linkedin}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <FaLinkedinIn
+                            className={`${styles.articleIcon} ${styles.linkedin}`}
+                          />
+                        </a>
+                      )}
                     </div>
                   </div>
                   <h1 className={styles.title}>{post.title}</h1>
@@ -155,30 +156,50 @@ const MainContent = ({ post }) => {
                     />
                   </div>
                   <div className={styles.articleFooter}>
-                    {/* <FaMeta
-                      className={`${styles.articleIcon} ${styles.meta}`}
-                    />
-                    <IoLogoWhatsapp
-                      className={`${styles.articleIcon} ${styles.whatsapp}`}
-                    />
-                    <FaXTwitter
-                      className={`${styles.articleIcon} ${styles.twitter}`}
-                    />
-                    <FaLinkedinIn
-                      className={`${styles.articleIcon} ${styles.linkedin}`}
-                    /> */}
-                      {/* <a href={shareUrls.meta} target="_blank" rel="noopener noreferrer">
-                        <FaMeta className={`${styles.articleIcon} ${styles.meta}`} />
+                    {shareUrls.meta && (
+                      <a
+                        href={shareUrls.meta}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <FaMeta
+                          className={`${styles.articleIcon} ${styles.meta}`}
+                        />
                       </a>
-                      <a href={shareUrls.whatsapp} target="_blank" rel="noopener noreferrer">
-                        <IoLogoWhatsapp className={`${styles.articleIcon} ${styles.whatsapp}`} />
+                    )}
+                    {shareUrls.whatsapp && (
+                      <a
+                        href={shareUrls.whatsapp}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <IoLogoWhatsapp
+                          className={`${styles.articleIcon} ${styles.whatsapp}`}
+                        />
                       </a>
-                      <a href={shareUrls.twitter} target="_blank" rel="noopener noreferrer">
-                        <FaXTwitter className={`${styles.articleIcon} ${styles.twitter}`} />
+                    )}
+                    {shareUrls.twitter && (
+                      <a
+                        href={shareUrls.twitter}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <FaXTwitter
+                          className={`${styles.articleIcon} ${styles.twitter}`}
+                        />
                       </a>
-                      <a href={shareUrls.linkedin} target="_blank" rel="noopener noreferrer">
-                        <FaLinkedinIn className={`${styles.articleIcon} ${styles.linkedin}`} />
-                      </a> */}
+                    )}
+                    {shareUrls.linkedin && (
+                      <a
+                        href={shareUrls.linkedin}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <FaLinkedinIn
+                          className={`${styles.articleIcon} ${styles.linkedin}`}
+                        />
+                      </a>
+                    )}
                   </div>
                 </div>
               </div>
