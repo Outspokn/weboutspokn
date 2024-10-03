4fb51ff3f4f7fe44428b9a99703a69606cbfe12a
Shruti <shrutigawande1910@gmail.com>
2024-10-03 22:17:25 +0530

    feat: About Section

---
+++ src/pages/about.js
@@ -0,0 +1,57 @@
+import React from "react";
+import styles from "../styles/about.module.css";
+import Image from "next/image";
+
+const about = () => {
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.leftContainer}>
+        <h2>How It Started</h2>
+        <h1>
+          Our Dream is <br /> Global Learning Transformation
+        </h1>
+        <p>
+          Kawruh was founded by Robert Anderson, a passionate lifelong learner,
+          and Maria Sanchez, a visionary educator. Their shared dream was to
+          create a digital haven of knowledge accessible to all. United by their
+          belief in the transformational power of education, they embarked on a
+          journey to build Kawruh. With relentless dedication, they gathered a
+          team of experts and launched this innovative platform, creating a
+          global community of eager learners, all connected by the desire to
+          explore, learn, and grow.
+        </p>
+      </div>
+      <div className={styles.rightContainer}>
+        <Image
+          src="/assets/laptop.jpg"
+          alt="Team Collaboration"
+          width={500}
+          height={300}
+          layout="fixed"
+          className={styles.image}
+        />
+
+        <div className={styles.lowerContainer}>
+          <div className={styles.statItem}>
+            <h3>3.5</h3>
+            <p>Years Experience</p>
+          </div>
+          <div className={styles.statItem}>
+            <h3>23</h3>
+            <p>Project Challenge</p>
+          </div>
+          <div className={styles.statItem}>
+            <h3>830+</h3>
+            <p>Positive Reviews</p>
+          </div>
+          <div className={styles.statItem}>
+            <h3>100K</h3>
+            <p>Trusted Students</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default about;
