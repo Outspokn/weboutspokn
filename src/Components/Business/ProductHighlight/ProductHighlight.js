992c62271210445faa3ea48bca8340e8612cb6f3
Shruti <shrutigawande1910@gmail.com>
2024-10-21 10:57:51 +0530

    style: Improved Responsiveness

--- src/Components/Business/ProductHighlight/ProductHighlight.js
+++ src/Components/Business/ProductHighlight/ProductHighlight.js
@@ -1,23 +1,31 @@
-import React from 'react'
-import styles from "./ProductHighlight.module.css"
+import React from "react";
+import styles from "./ProductHighlight.module.css";
+import Image from "next/image";
 
 const ProductHighlight = () => {
   return (
     <div className={styles.mainContainer}>
-    <div className={styles.textSection}>
-      <h2>Anything, anywhere, anytime.</h2>
-      <p>
-        Outspokn, your always-there AI tutor, offers a safe, judgement-free zone for practicing English. It’s dedicated
-        to empowering you every step of the way, provides feedback and tools to boost your confidence, making your
-        path to English mastery both effective and fast.
-      </p>
+      <div className={styles.textSection}>
+        <h2>Anything, anywhere, anytime.</h2>
+        <p>
+          Outspokn, your always-there AI tutor, offers a safe, judgement-free
+          zone for practicing English. It’s dedicated to empowering you every
+          step of the way, provides feedback and tools to boost your confidence,
+          making your path to English mastery both effective and fast.
+        </p>
+      </div>
+      <div className={styles.imageSection}>
+        <Image
+          src="https://outspoknweb.s3.us-east-2.amazonaws.com/header/HeaderMobile.png"
+          alt="Header Image"
+          className={styles.chatImage}
+          width={300}
+          height={533}
+          priority
+        />
+      </div>
     </div>
-    <div className="image-section">
-   
-    </div>
-  </div>
-  )
-}
+  );
+};
 
-export default ProductHighlight
-
+export default ProductHighlight;
