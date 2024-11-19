008751b7c19dcb9de16cbcc7d270e6e07e8a5ec6
somanath khadanga <31034311+somnathraz@users.noreply.github.com>
2024-11-19 11:04:22 +0530

    GTM addex

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,6 @@ import { Nunito_Sans } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import "dotenv/config";
 import { GoogleTagManager } from "@next/third-parties/google";
-import Script from "next/script";
 
 const nunitoSans = Nunito_Sans({
   subsets: ["latin"],
@@ -14,22 +13,10 @@ const nunitoSans = Nunito_Sans({
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Script
-        id="gtm-script"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','G-F1PNKZRHQH');
-          `,
-        }}
-      />
       <main className={nunitoSans.className}>
         <SpeedInsights />
         <Component {...pageProps} />
+        <GoogleTagManager gtmId="GTM-WSGWSB2W" />
       </main>
     </>
   );
