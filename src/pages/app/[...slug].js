76517f3bd25ce2d1e414232c09d2eeb8e6be5974
somanath khadanga <31034311+somnathraz@users.noreply.github.com>
2024-12-17 16:55:35 +0530

    adding redirection tracking

--- src/pages/app/[...slug].js
+++ src/pages/app/[...slug].js
@@ -1,12 +1,25 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import { sendGTMEvent } from "@next/third-parties/google";
 
 const AppPage = () => {
   const router = useRouter();
 
   useEffect(() => {
-    const { slug } = router.query;
+    const { slug, utm_source, utm_campaign } = router.query;
+
     const screen = slug ? slug.join("/") : "";
+    // 1. Trigger Google Analytics Event
+    sendGTMEvent({
+      event: "user_redirect",
+      category: "DeepLink",
+      label: "App Redirection",
+      data: {
+        screen: screen || "home",
+        utm_source: utm_source || "unknown",
+        utm_campaign: utm_campaign || "default",
+      },
+    });
     window.location.href = `/api/deeplink?screen=${screen}`;
   }, [router.query]);
 
