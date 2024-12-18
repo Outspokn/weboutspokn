648ac6f6b46b8ed3e3a58b255d1f8ce957aa4de4
somanath khadanga <31034311+somnathraz@users.noreply.github.com>
2024-12-18 09:55:47 +0530

    removed custom scheme

--- src/pages/api/deeplink.js
+++ src/pages/api/deeplink.js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
   const { screen } = req.query;
 
   // Construct the deep link URL for your app
-  const deepLink = `outspokn://app/${screen || ""}`;
+  // const deepLink = `outspokn://app/${screen || ""}`;
 
   // Store URLs
   const appStoreUrl = "https://apps.apple.com/us/app/outspokn/id6737053468";
@@ -32,20 +32,21 @@ export default function handler(req, res) {
     `);
   } else if (isAndroid) {
     // Attempt to open the app for Android
+    const universalLink = `https://www.outspokn.ai/app/${screen || ""}`;
     res.send(`
-        <html>
-          <head>
-            <meta http-equiv="refresh" content="1;url=${deepLink}" />
-          </head>
-          <body>
-            <script>
-              setTimeout(() => {
-                window.location.href = "${playStoreUrl}";
-              }, 2000);
-            </script>
-          </body>
-        </html>
-      `);
+      <html>
+        <head>
+          <meta http-equiv="refresh" content="1;url=${universalLink}" />
+        </head>
+        <body>
+          <script>
+            setTimeout(() => {
+              window.location.href = "${playStoreUrl}";
+            }, 2000);
+          </script>
+        </body>
+      </html>
+    `);
   } else {
     // Redirect unsupported platforms to a fallback webpage
     res.redirect("/");
